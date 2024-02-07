730d4b2efcd3c506c84f07679cb5bcd3fe4064a5
QueryInterface <volkov.stanislav@gmail.com>
2024-02-07 10:43:45 +0000

    Rename of files and addition of few options

--- windows-11/setup-platform-configuration.ps1
+++ windows-11/setup-platform-configuration.ps1
@@ -16,13 +16,13 @@ function Create-Hardlink-To-Config-Files {
     Write-Host "$verboseMessage... DONE"
 }
 
-Create-Hardlink-To-Config-Files -sourceConfigPath ".\.vsvimrc" `
+Create-Hardlink-To-Config-Files -sourceConfigPath ".\vs-2022-vim-settings" `
                                 -destinationConfigPath "C:\Users\$username\.vsvimrc" `
                                 -verboseMessage "Setting up VsVim for Visual Studio"
-Create-Hardlink-To-Config-Files -sourceConfigPath "XYplorer.ini" `
+Create-Hardlink-To-Config-Files -sourceConfigPath "xyplorer-settings.ini" `
                                 -destinationConfigPath "$appdata\XYplorer\XYplorer.ini" `
                                 -verboseMessage "Setting up XYplorer configuration"
-Create-Hardlink-To-Config-Files -sourceConfigPath "settings.json" `
+Create-Hardlink-To-Config-Files -sourceConfigPath "vs-code-settings.json" `
                                 -destinationConfigPath "$appdata\Code\User\settings.json" `
                                 -verboseMessage "Setting up VS Code configuration"
 
