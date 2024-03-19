c78509db7461ddf48d30c1bd5695b9e48bdd265d
QueryInterface <volkov.stanislav@gmail.com>
2024-03-19 14:01:59 +0000

    Returned option to create hard links
    XYPlorer config load was broken otherwise

--- windows-11/setup-platform-configuration.ps1
+++ windows-11/setup-platform-configuration.ps1
@@ -25,16 +25,32 @@ function Create-SymbolLink-To-Config-Files {
     Write-Host "$verboseMessage... DONE"
 }
 
+function Create-HardLink-To-Config-Files {
+    param (
+        [string]$sourceConfigPath,
+        [string]$destinationConfigPath,
+        [string]$verboseMessage
+    )
+    Write-Host "$verboseMessage"
+    if (-not (Test-Path $sourceConfigPath)) 
+    {
+        Write-Error "The source path '$sourceConfigPath' does not exist."
+    } 
+    $absoluteSourceConfigPath = Resolve-Path -Path $sourceConfigPath
+    New-Item -ItemType HardLink -Path $destinationConfigPath -Target $absoluteSourceConfigPath -Force
+    Write-Host "$verboseMessage... DONE"
+}
+
 Create-SymbolLink-To-Config-Files -sourceConfigPath ".\vs-2022-vim-settings" `
                                 -destinationConfigPath "C:\Users\$username\.vsvimrc" `
                                 -verboseMessage "Setting up VsVim for Visual Studio"
-Create-SymbolLink-To-Config-Files -sourceConfigPath ".\xyplorer-settings.ini" `
+Create-HardLink-To-Config-Files -sourceConfigPath ".\xyplorer-settings.ini" `
                                 -destinationConfigPath "$appdata\XYplorer\XYplorer.ini" `
                                 -verboseMessage "Setting up XYplorer configuration"
 Create-SymbolLink-To-Config-Files -sourceConfigPath ".\vs-code-settings.json" `
                                 -destinationConfigPath "$appdata\Code\User\settings.json" `
                                 -verboseMessage "Setting up VS Code configuration"
-Create-SymbolLink-To-Config-Files -sourceConfigPath ".\autohotkey-settings.ahk" `
+Create-HardLink-To-Config-Files -sourceConfigPath ".\autohotkey-settings.ahk" `
                                 -destinationConfigPath "C:\Users\$username\autohotkey-settings.ahk" `
                                 -verboseMessage "Setting up AutoHotKey configuration"
 Create-SymbolLink-To-Config-Files -sourceConfigPath ".\power-toys-keyboard-mappings.json" `
