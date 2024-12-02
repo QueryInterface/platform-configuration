4e7704494a5e362ade83bf1f36a48965a86782c5
QueryInterface <volkov.stanislav@gmail.com>
2024-12-02 19:24:10 +0000

    The most recent version

--- windows-11/setup-platform-configuration.ps1
+++ windows-11/setup-platform-configuration.ps1
@@ -2,6 +2,8 @@ $username = $env:UserName
 $appdata = $env:AppData
 $documents = [Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)
 
+Set-ExecutionPolicy Unrestricted
+
 function Run-With-Admin-Privileges {
     if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
         # Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
